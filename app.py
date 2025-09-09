465aa84396f8ca2e6d57e78b917db9c9e8f44d08
Nithin <nithinmanja92@gmail.com>
2025-09-09 16:12:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ notes = []
 
 @app.route("/")
 def home():
-    return "📝 Welcome to Notes App APIsss fater making changes by AFNAN.again....."
+    return "📝 Welcome to Notes App APIsss fater making changes by AFNAN.again..enabled POLL SCM now..."
 
 @app.route("/notes", methods=["GET"])
 def get_notes():
